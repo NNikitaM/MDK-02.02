4fb7abcefc0f48f0f897115665751590660eabe9
nikita <nikitamatinin152@gmail.com>
2024-09-03 16:01:06 +0300

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int numberOffEggs = 3;
+        if (numberOffEggs > 3)
+            System.out.println("Рекомендую приготовить омлет.");
+        else
+            System.out.println("Рекомендую позавтракать бутербродами.");
+        System.out.println("Приятного аппетита!");
+    }
+}
